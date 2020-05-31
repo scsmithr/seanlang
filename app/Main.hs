8f57b7105ff442498c252b2af4f6898ae74a8c21
Sean Smith <scsmithr@gmail.com>
2020-05-30 20:27:01 -0500

    Add repl

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,35 @@ module Main where
 import           Parser
 import           Control.Monad
 import           System.Environment
+import           System.IO
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = case readExpr expr >>= eval of
+  Left  err -> return $ "Error: " ++ show err
+  Right val -> return $ show val
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result then return () else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 main :: IO ()
 main = do
-  args   <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  case evaled of
-    Left  err    -> putStrLn $ "Error: " ++ show err
-    Right evaled -> putStrLn evaled
+  args <- getArgs
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint (args !! 0)
+    _ -> putStrLn "0 or 1 args"
+
